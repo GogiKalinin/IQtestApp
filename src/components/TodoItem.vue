db685bf6a9aca419406c68abc639d3785169979a
GogiKalinin <bbdostoevskij@gmail.com>
2023-04-17 22:58:26 +0300

    create simple todo app

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+<ul>
+    <li>
+        <span v-bind:class="{done: todo.completed}">
+            <input type="checkbox" v-on:change="todo.completed = !todo.completed">
+            <strong>{{index + 1}}</strong>
+            {{todo.title}}
+        </span>
+        <button class="rm" 
+            v-on:click="$emit('remove-todo', todo.id)"
+        >&times;</button>
+    </li>
+</ul>
+</template>
+
+<script>
+    export default {
+        props: {
+            todo: {
+                type: Object,
+                required: true
+            },
+        index: Number
+        }
+    }
+</script>
+
+<style scoped>
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5em 2rem;
+        margin-bottom: 1rem;
+    }
+
+    .rm {
+        background: red;
+        color: #fff;
+        border-radius: 50%;
+        font-weigth: bold;
+    }
+
+    input {
+        margin-right: 1rem;
+    }
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Home page</h1>
+        <p>lorem10</p>
+        <!-- <a href="/todos">Todos</a> -->
+    </div>
+</template>
